d79b514732d2eeceec4b7748735baf4be9c0f209
spiel0meister <soviczan7@gmail.com>
2024-01-30 18:37:36 +0100

    added trouble.nvim

--- lua/spiel_meister/packer.lua
+++ lua/spiel_meister/packer.lua
@@ -116,4 +116,22 @@ return require('packer').startup(function(use)
     use "lukas-reineke/lsp-format.nvim"
 
     use "nvim-treesitter/playground"
+
+    use {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
+            vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
+            vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
+            vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
+            vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+            vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+        end
+    }
 end)
