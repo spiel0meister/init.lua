2bf55a5064e4a44a7583e3589ef4837212a897d7
spiel0meister <soviczan7@gmail.com>
2024-02-01 18:50:39 +0100

    changes

--- after/plugin/codeium.lua
+++ after/plugin/codeium.lua
@@ -1,6 +1,6 @@
 -- local codeium = require("codeium")
 
-vim.keymap.set("i", "<c-tab>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+-- vim.keymap.set("i", "<c-tab>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
 vim.keymap.set("i", "<c-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
 vim.keymap.set("i", "<c-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
 vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
