6baa75e2709bd0316f682f6e19dceabdad05fe20
spiel0meister <soviczan7@gmail.com>
2024-02-18 01:01:04 +0100

    better setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-apt install unzip
+exit_if_fail() {
+    if [[ $? != 0 ]]; then
+        exit $?
+    fi
+}
+
+apt install unzip > /dev/null
 
 # Install packer
 if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
@@ -14,27 +20,36 @@ fi
 
 if [ ! -d ~/.local/share/fonts/JetBrainsMono ]; then
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-    unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
-    fc-cache -f -v
+    exit_if_fail
+    unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono > /dev/null
+    exit_if_fail
+    fc-cache -f -v /dev/null
     rm -rf JetBrainsMono.zip
+    echo "Installed JetBrainsMono Nerd Font"
 fi
 
 # Install Rust
 if [ ! -f $HOME/.cargo/bin/rustc ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "Installed Rust"
+else
+    echo "Rust already installed"
 fi
 
-apt install cargo
+apt install cargo > /dev/null
 
 # Install Haskell
-apt install build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
+apt install build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 > /dev/null
 
 if [ ! -f $HOME/.ghcup/bin/ghc ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+    echo "Installed Haskell"
+else
+    echo "Haskell already installed"
 fi
 
 # Install gcc
-apt install gcc
+apt install gcc > /dev/null
 
 # Install NodeJS and Typescript
 if [ ! -f /home/zan-sovic/.local/share/fnm/fnm ]; then
@@ -53,6 +68,9 @@ apt install -y curl \
     libbz2-dev libsndio-dev freeglut3-dev libxmu-dev libxi-dev libfontconfig1-dev \
     libxcursor-dev
 
-if [ ! -d /home/zan-sovic/.cargo/bin/neovide ]; then
+echo "Install Neovide? (y/N) "
+read -r ANSWER
+
+if [[ $ANSWER == "y" ]]; then
     cargo install --git https://github.com/neovide/neovide
 fi
