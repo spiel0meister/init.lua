d546da264892ab266ea7cf67ffdd485bcf9cfe14
spiel0meister <soviczan7@gmail.com>
2024-02-25 19:16:23 +0100

    added keymaps; comments

--- lua/spiel_meister/remap.lua
+++ lua/spiel_meister/remap.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
+
+-- Exit file, open file tree
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Windows
+vim.keymap.set("n", "<leader>w", "<C-w>")
+vim.keymap.set("n", "<leader>sv", "<C-w><C-v>")
+vim.keymap.set("n", "<leader>sh", "<C-w>s")
+vim.keymap.set("n", "<leader>cw", "<C-w>c")
+vim.keymap.set("n", "<leader>wo", "<C-w>o")
+vim.keymap.set("n", "<leader>w=", "<C-w>=")
+
+-- Saving
+vim.keymap.set("n", "<leader>s", ":w<CR>")
+vim.keymap.set("n", "<C-s>", "<C-c>:w<CR>a")
