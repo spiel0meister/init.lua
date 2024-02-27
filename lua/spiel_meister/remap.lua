6c2cf91261d789b7966da5a5de1497964a21878c
spiel0meister <soviczan7@gmail.com>
2024-02-27 01:15:53 +0100

    keymap; keymap comments explaining keymaps

--- lua/spiel_meister/remap.lua
+++ lua/spiel_meister/remap.lua
@@ -1,33 +1,38 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>", "")
 
--- Exit file, open file tree
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Exit file, open file tree
+
+-- Git
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- Open Git (vim-fugitive) (probably)
+
+-- Terminal
+vim.keymap.set("n", "<leader>to", ":term<CR>") -- Open terminal emulator
 
 -- Exotic motions
-vim.keymap.set("n", "<leader>jb", "%")
+vim.keymap.set("n", "<leader>jb", "%") -- Jump to next pair
 
 -- Windows
-vim.keymap.set("n", "<leader>w", "<C-w>")
-vim.keymap.set("n", "<leader>sv", "<C-w><C-v>")
-vim.keymap.set("n", "<leader>sh", "<C-w>s")
-vim.keymap.set("n", "<leader>cw", "<C-w>c")
-vim.keymap.set("n", "<leader>kw", "<C-w>o")
-vim.keymap.set("n", "<leader>=w", "<C-w>=")
+vim.keymap.set("n", "<leader>w", "<C-w>")       -- Don't have time to press Ctrl+w
+vim.keymap.set("n", "<leader>sv", "<C-w><C-v>") -- Split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s")     -- Split window horizontally
+vim.keymap.set("n", "<leader>cw", "<C-w>c")     -- Close window but keep buffer
+vim.keymap.set("n", "<leader>kw", "<C-w>o")     -- Close all but selected window
+vim.keymap.set("n", "<leader>=w", "<C-w>=")     -- Re size windows to equal size
 
 -- Saving
-vim.keymap.set("n", "<leader>sf", ":w<CR>")
-vim.keymap.set("i", "<C-s>", "<C-c>:w<CR>a")
+vim.keymap.set("n", "<leader>sf", ":w<CR>")  -- Save file
+vim.keymap.set("i", "<C-s>", "<C-c>:w<CR>a") -- Save file in insert mode
 
 -- Yanking and Pasting
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+y")  -- Set up yanking into clipboard
+vim.keymap.set("v", "<leader>y", "\"+y")  -- Yank selection into clipboard
 
-vim.keymap.set("n", "<leader>pp", "\"*p")
+vim.keymap.set("n", "<leader>pp", "\"*p") -- Paste clipboard
 
 -- Formatting
-vim.keymap.set("n", "<leader>f", function()
+vim.keymap.set("n", "<leader>f", function() -- Reformat file (with LSP)
     vim.lsp.buf.format()
 end)
-vim.keymap.set("n", "<leader>rt", ">>")
-vim.keymap.set("n", "<leader>lt", "<<")
+vim.keymap.set("n", "<leader>rt", ">>") -- Ident right
+vim.keymap.set("n", "<leader>lt", "<<") -- Ident left
