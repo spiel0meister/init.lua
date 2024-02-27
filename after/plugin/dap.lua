32e86cf88455aae76d3b8567fd3e09c8f2022a8e
spiel0meister <soviczan7@gmail.com>
2024-02-27 20:27:48 +0100

    added dap

---
+++ after/plugin/dap.lua
@@ -0,0 +1,21 @@
+local dap = require("dap")
+local dap_ui = require("dapui")
+local dap_py = require("dap-python")
+dap_ui.setup({})
+dap_py.setup()
+
+vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<leader>dc", dap.continue, {})
+
+dap.listeners.before.attach.dapui_config = function()
+    dap_ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+    dap_ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+    dap_ui.open()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+    dap_ui.open()
+end
