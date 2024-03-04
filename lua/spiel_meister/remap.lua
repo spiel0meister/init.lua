32a384eebecfb677eeb1391860c98c9baab2ffee
spiel0meister <soviczan7@gmail.com>
2024-03-04 21:17:26 +0100

    : -> <CMD>; added diagnostics keymaps

--- lua/spiel_meister/remap.lua
+++ lua/spiel_meister/remap.lua
@@ -3,11 +3,14 @@ vim.keymap.set("n", "<leader>", "")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Open explorer
 
+-- Diagnostics
+vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float) -- Open diagnostics Error Messages
+vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist) -- Open diagnostics Quick fixes
+vim.keymap.set("n", "<leader>dgn", vim.diagnostic.goto_next) -- Go to next diagnostics 
+vim.keymap.set("n", "<leader>dgp", vim.diagnostic.goto_prev) -- Go to previous diagnostics
+
 -- Git
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- Open Git (vim-fugitive) (probably)
-
--- Terminal
-vim.keymap.set("n", "<leader>to", ":term<CR>") -- Open terminal emulator
 
 -- Exotic motions
 vim.keymap.set("n", "<leader>jb", "%") -- Jump to next pair
@@ -21,8 +24,8 @@ vim.keymap.set("n", "<leader>kw", "<C-w>o")     -- Close all but selected window
 vim.keymap.set("n", "<leader>=w", "<C-w>=")     -- Re size windows to equal size
 
 -- Saving
-vim.keymap.set("n", "<leader>sf", ":w<CR>")  -- Save file
-vim.keymap.set("i", "<C-s>", "<C-c>:w<CR>a") -- Save file in insert mode
+vim.keymap.set("n", "<leader>sf", "<CMD>w<CR>")  -- Save file
+vim.keymap.set("i", "<C-s>", "<CMD>w<CR>") -- Save file in insert mode
 
 -- Yanking and Pasting
 vim.keymap.set("n", "<leader>y", "\"+y")  -- Set up yanking into clipboard
